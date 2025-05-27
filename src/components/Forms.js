274f98ab5585d6c851758867b2fb27c693c51455
Srbaqua <23bcs101@nith.ac.in>
2025-05-27 22:21:31 +0530

    Priority option added

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -5,24 +5,40 @@ const Forms = () => {
     <div className="forms">
         <form className="row gy-2 gx-3 align-items-center">
   <div className="col-auto">
-    <label className="visually-hidden" for="autoSizingInput">Name</label>
+    <label for="autoSizingInput">Process</label>
     <input type="text" className="form-control" id="processName" placeholder='Process Name'/>
   </div>
   <div className="col-auto">
-    <label className="visually-hidden" for="autoSizingInputGroup">Username</label>
+    <label for="autoSizingInputGroup">Arrival Time</label>
     <div className="input-group">
       <div className="input-group-text"></div>
       <input type="text" className="form-control" id="autoSizingInputGroup"/>
     </div>
   </div>
   <div className="col-auto">
-    <label className="visually-hidden" for="autoSizingSelect">Preference</label>
-    <select className="form-select" id="autoSizingSelect">
-      <option selected>Choose...</option>
-      <option value="1">FCFS</option>
-      <option value="2">SJF</option>
-      <option value="3">ROUND ROBIN</option>
-    </select>
+    <label for="autoSizingInputGroup">Burst Time</label>
+    <div className="input-group">
+      <div className="input-group-text"></div>
+      <input type="text" className="form-control" id="autoSizingInputGroup"/>
+    </div>
+  </div>
+  <div className="col-auto">
+    <label  for="autoSizingSelect">Scheduling Method</label>
+    <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+    <option selected>Choose method</option>
+    <option value="1">FCFS</option>
+    <option value="2">SJF</option>
+    <option value="3">Round Robin</option>
+  </select>
+  </div>
+  <div className="col-auto">
+    <label  for="autoSizingSelect">Priority</label>
+    <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+    <option selected>Choose Priority order</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+  </select>
   </div>
   <div className="col-auto">
     <div className="form-check">
