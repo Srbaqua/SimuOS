ef090b626e0613119343daecda888d70439be9a4
Srbaqua <23bcs101@nith.ac.in>
2025-08-06 00:33:56 +0530

    atlas error resolved

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,10 @@ app.use(cors({
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
   
 
 
