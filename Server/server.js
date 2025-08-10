e007c52309b5c78fde468d0137b47b85fdc3a1aa
Srbaqua <23bcs101@nith.ac.in>
2025-08-10 12:53:18 +0530

    update link

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://simu-kgc3k9rfj-srbaquas-projects.vercel.app'
+    'https://simu-os-project.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
